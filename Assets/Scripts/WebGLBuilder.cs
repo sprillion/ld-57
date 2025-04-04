c7911e2a380ee1d08e93f23074e533103bcda27d
sprill <sergei.sprill@gmail.com>
2025-04-05 02:44:40 +0500

    Test

---
+++ Assets/Scripts/WebGLBuilder.cs
@@ -0,0 +1,13 @@
+﻿using UnityEditor;
+
+public class WebGLBuilder {
+    [MenuItem("Build/WebGL")]
+    public static void Build() {
+        BuildPipeline.BuildPlayer(
+            levels: new[] { "Assets/Scenes/Game.unity" }, // Укажите ваши сцены
+            locationPathName: "webgl_build",
+            target: BuildTarget.WebGL,
+            options: BuildOptions.None
+        );
+    }
+}
